282ca08625f1303dfefecae0f546594161c7587c
Joaquin Freire <ernestojoaquinfreire@gmail.com>
2023-05-01 10:43:58 -0300

    Creación de register y maquetado

--- src/pages/Registrer.jsx
+++ src/pages/Registrer.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import '../style/register.css';
 
-const Registrer = () => {
-  return (
-    <div>Registrer</div>
-  )
+export const Registrer = () => {
+    return (
+        <>
+            <form className='formulario' action="">
+                <h1 className='titulo'>User Registration</h1>
+                <div className='contenedor-info'>
+                    <div className='label-input'>
+                        <label htmlFor="email">E-mail:</label>
+                        <input type="email" placeholder="Enter your email" />
+                    </div>
+                    <div className='flexear'>
+                        <div className='label-input'>
+                            <label htmlFor="name">Name:</label>
+                            <input type="text" placeholder="Enter your name" />
+                        </div>
+                        <div className='label-input'>
+                            <label htmlFor="lastname">Last name:</label>
+                            <input type="text" placeholder="Enter your last name" />
+                        </div>
+                    </div>
+                    <div className='flexear'>
+                        <div className='label-input'>
+                            <label htmlFor="name">Password:</label>
+                            <input type="password" placeholder="Enter your password" />
+                        </div>
+                        <div className='label-input'>
+                            <label htmlFor="name">Confirm Password:</label>
+                            <input type="password" placeholder="Confirm Password" />
+                        </div>
+                    </div>
+                    <div className='consultas'>
+                        <input className='registrar' type="submit" value='Register' />
+                        <a className='iniciar' href="#">Already have an account? Sign In</a>
+                    </div>
+                </div>
+
+            </form>
+        </>
+    )
 }
-
-export default Registrer
