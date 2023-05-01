02ffe0db6763710805dec8a107fd7f9349321a34
Joaquin Freire <ernestojoaquinfreire@gmail.com>
2023-05-01 10:42:34 -0300

    Creación de login y maquetado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import '../style/login.css'
 
-const Login = () => {
+export const Login = () => {
   return (
-    <div>Login</div>
+    <section>
+      <form className='formulario' action="">
+        <h1 className='titulo'>User Login</h1>
+        <div className='contenedor-info'>
+          <div className='label-input'>
+            <label htmlFor="email">E-mail:</label>
+            <input type="email" placeholder="Enter your email" />
+          </div>
+          <div className='label-input'>
+            <label htmlFor="email">Password:</label>
+            <input type="password" placeholder="Enter your password" />
+          </div>
+          <div className='consultas'>
+            <input className='registrar' type="submit" value='Login' />
+            <button className='olvide' value='Login'>I forgot my password</button>
+          </div>
+        </div>
+      </form>
+      <a className='no-registrado' href="#">Don't have an account? Register</a>
+    </section>
   )
 }
-
-export default Login
