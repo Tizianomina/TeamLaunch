293fcb5b25f650fb9253b33c70fce0b9e1f60eaf
Tiziano Joaquín Mina <tizianominajo@gmail.com>
2023-05-03 12:04:39 -0300

    Creación del componente Proyecto

---
+++ src/components/Proyect.jsx
@@ -0,0 +1,17 @@
+export const Proyect = () => {
+  return (
+    <article className="proyecto">
+      <img
+        className="proyecto-img"
+        src="https://picsum.photos/200/300"
+        alt=""
+      />
+      <div className="proyecto-info">
+        <p className="proyecto-nombre">Nombre del Proyecto</p>
+        <a href="" className="proyecto-link">
+          Ver
+        </a>
+      </div>
+    </article>
+  );
+};
